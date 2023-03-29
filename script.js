642b0a60fd9f8fcdcb4510c565d0f3ac0ada9c3f
Bisi0n <110070415+Bisi0n@users.noreply.github.com>
2023-03-29 06:39:48 +0200

    getPercentage fix

--- script.js
+++ script.js
@@ -129,72 +129,48 @@ const app = Vue.createApp({
     },
 
     getFoodExpenses() {
-
       let total = 0;
 
       for (let expense of this.expenses) {
-
         if (expense.category === 'Food') {
-
           total += parseInt(expense.expense);
-
         }
-
       }
-
       return total;
-
     },
 
     getRentExpenses() {
-
       let total = 0;
 
       for (let expense of this.expenses) {
-
         if (expense.category === 'Rent') {
-
           total += parseInt(expense.expense);
-
         }
-
       }
-
       return total;
-
     },
 
     getTransportExpenses() {
-
       let total = 0;
 
       for (let expense of this.expenses) {
-
         if (expense.category === 'Transport') {
-
           total += parseInt(expense.expense);
-
         }
-
       }
-
       return total;
-
     },
     drawDiagram() {
       const canvas = this.$refs.canvas;
       const ctx = canvas.getContext('2d');
 
       if (canvas === undefined) {
-
         return;
-
       }
 
       ctx.clearRect(0, 0, canvas.width, canvas.height);
 
       const w = canvas.width = 600;
-
       const h = canvas.height = 500;
 
       let food = (this.getFoodExpenses() / this.displayAllExpense()) * 360;
@@ -203,50 +179,29 @@ const app = Vue.createApp({
       let rent = (this.getRentExpenses() / this.displayAllExpense()) * 360;
       rent.toFixed(0);
 
-
       let transport = (this.getTransportExpenses() / this.displayAllExpense()) * 360;
       transport.toFixed(0);
 
-
-
       let angels = [food * Math.PI / 180, rent * Math.PI / 180, transport * Math.PI / 180];
-
       let colors = ['#4CAF50', '#00BCD4', '#E91E63'];
-
       let startAngel = 0;
-
       let endAngel = 0;
-
       let cx = w / 2;
-
       let cy = h / 2;
 
-
       for (let i = 0; i < angels.length; i++) {
-
         startAngel = endAngel;
-
         endAngel += angels[i];
 
         ctx.beginPath();
-
         ctx.fillStyle = colors[i % colors.length]
-
-
         ctx.moveTo(cx, cy);
-
         ctx.arc(cx, cy, 150, startAngel, endAngel);
-
         ctx.lineTo(cx, cy);
-
         ctx.stroke();
-
         ctx.fill();
-
-
       }
     },
-
   },
   computed: {
     filteredExpenses() {
@@ -265,6 +220,30 @@ const app = Vue.createApp({
         return (!this.startDate || incomeDate >= startDate) &&
           (!this.endDate || incomeDate <= endDate);
       });
+    },
+
+    // getCategoryPercentages() {
+    //   const categoryTotals = {};
+    //   this.expenses.forEach(expense => {
+    //     if (!categoryTotals[expense.category]) {
+    //       categoryTotals[expense.category] = 0;
+    //     }
+    //     categoryTotals[expense.category] += expense.expense;
+    //   });
+    
+    //   const totalAmount = Object.values(categoryTotals).reduce((total, amount) => total + amount, 0);
+    
+    //   return Object.entries(categoryTotals).map(([category, amount]) => ({
+    //     category,
+    //     percentage: ((amount / totalAmount) * 100).toFixed(2),
+    //   }));
+    // },
+    
+    totalAmount() {
+      return this.expenses.reduce((total, expense) => total + expense.expense, 0);
+    },
+    getPercentage() {
+      return (expense) => ((expense.expense / this.totalAmount) * 100).toFixed(2);
     },
 
   },
